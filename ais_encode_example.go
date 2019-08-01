ce7a194ff774c4a4b739e92de279df1d9712f903
Arianov Sergey Sergeevi4 <ssarianov@sberbank.ru>
2019-08-01 11:58:32 +0300

    Added 24 message type

--- ais_encode_example.go
+++ ais_encode_example.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	//TYPE 1
 	var mmsi int = 247320162
 	var speed float64 = 0.1
 	var longtitude float64 = 5.6
@@ -14,7 +15,27 @@ func main() {
 	var course float64 = 273.1
 	//Timestamp (sec) integer
 	var ts int = 1
-
+	
+	fmt.Println("TYPE 1")
 	binstr := marinelib.EncodeType1(mmsi, speed, longtitude, latitude, course, ts)
 	fmt.Println(marinelib.GenerateNMEA(binstr))
+	
+	//TYPE 24A
+	fmt.Println("TYPE 24A")
+	vesselName := "VSHIP1"
+	binstr = marinelib.EncodeType24(mmsi, "A", vesselName , "", "", 0)
+	fmt.Println(marinelib.GenerateNMEA(binstr))
+
+	
+	callSign := "CASN1"
+	vesselDimensions := "20x10"
+
+	//Vessel Type and Cargo
+	passengers := 100
+	
+	//TYPE 24B
+	fmt.Println("TYPE 24B")
+	binstr = marinelib.EncodeType24(mmsi, "B", "" , callSign, vesselDimensions, passengers)
+	fmt.Println(marinelib.GenerateNMEA(binstr))	
+
 }
